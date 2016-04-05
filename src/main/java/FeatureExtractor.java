86f300d167115f9c5b78b176aeb31437b6777a8e
Yacine Mekesser <ymekesser@hotmail.com>
2016-04-05 15:51:50 +0200

    Initial Commit for project structure.

---
+++ src/main/java/FeatureExtractor.java
@@ -0,0 +1,11 @@
+/**
+ * Created on 05.04.2016.
+ *
+ * @author ymeke
+ */
+public class FeatureExtractor {
+
+    public static void main(String[] args) {
+        // Hier kommt der Start-Code rein
+    }
+}
