743bbccba52bc45cc658450a77e5e11da6bf16a2
Tobias Meier <tobias@meier.name>
2016-04-05 16:37:17 +0200

    Projektstruktur angepasst

--- src/main/java/FeatureExtractor.java
+++ src/main/java/FeatureExtractor.java
@@ -8,6 +8,7 @@ public class FeatureExtractor {
     public static void main(String[] args) {
         // Hier kommt der Start-Code rein
 
+        // CLI Parameter und Config aus .cfg File einlesen
         // Relevante Fileversionen aus DB holen
         // Für jede Fileversion:
         //  - File/Source-Code aus git holen (repository-package)
---
+++ src/main/java/utils/Config.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Created on 05.04.2016.
+ *
+ * @author tobias.meier
+ */
+public class Config {
+    // Config Parsing
+}
