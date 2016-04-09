dc9ef88648408b4834f9fa2e4d5a20a3fd5563c7
Tobias Meier <tobias@meier.name>
2016-04-09 09:37:11 +0200

    Git Teil implementiert

--- src/main/java/FeatureExtractor.java
+++ src/main/java/FeatureExtractor.java
@@ -1,5 +1,8 @@
 import repository.Git;
 import utils.Config;
+
+import java.io.FileNotFoundException;
+
 /**
  * Created on 05.04.2016.
  *
@@ -20,8 +23,13 @@ public class FeatureExtractor {
         cfg.readConfigFile();
 
 
-        Git git = new Git(1);
-        git.getSourceCode("","d347d2939fb04d4634a398b6d331dab6acfd75f4");
+        Git git = new Git("P:\\Studium\\FS2016\\BA\\GitHubProjects\\LED-Cube-Prototyper");
+        try {
+            char[] code = git.getSourceCode("Controller.py","31b6d396ba14fcb0e61650937f5d1754c10958bf");
+            System.out.println(code);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         //  - File/Source-Code aus git holen (repository-package)
         //  - AST aus Code parsen
         //  - Alle Feature Extractors drüberlaufen lassen
--- src/main/java/repository/Git.java
+++ src/main/java/repository/Git.java
@@ -1,13 +1,24 @@
 package repository;
 
+import org.eclipse.jgit.errors.CorruptObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.apache.commons.io.IOUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -21,9 +32,9 @@ public class Git {
     Repository repository;
 
 
-    public Git(int repositoryId){
+    public Git(String path){
 
-        String repositoryPath = "P:\\Studium\\FS2016\\BA\\GitHubProjects\\LED-Cube-Prototyper";
+        String repositoryPath = path;
         String gitPath = repositoryPath + "\\.git";
 
         repositoryBuilder.setMustExist(true);
@@ -35,21 +46,59 @@ public class Git {
         }
     }
 
-    public char[] getSourceCode(String path, String commitSHA) {
-        // hier kommt cooode rein!
+    public char[] getSourceCode(String path, String commitSHA) throws FileNotFoundException {
         char[] code = null;
 
-        RevWalk walk = new RevWalk(repository);
-        ObjectId commitId = ObjectId.fromString(commitSHA);
+        Map<String, ObjectId> files = getFiles(commitSHA);
+
+        if ( !files.containsKey(path) )
+            throw new FileNotFoundException("file " + path + " not found in commit " + commitSHA);
+
         try {
-            RevCommit commit = walk.parseCommit(commitId);
-            System.out.print(commit);
-            walk.dispose();
+            ObjectId file = files.get(path);
+            ObjectLoader loader = repository.open(file);
+            InputStream in = loader.openStream();
+            String codeString = IOUtils.toString(in, "utf8");
+            code = codeString.toCharArray();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return code;
+    }
+    private Map<String, ObjectId> getFiles(String commitSHA) {
+
+        Map<String, ObjectId> files = new HashMap<String, ObjectId>();
+
+        RevWalk walk = new RevWalk(repository);
+        ObjectId commitId = ObjectId.fromString(commitSHA);
+        try {
+            RevCommit commit = walk.parseCommit(commitId);
+
+            RevTree tree = commit.getTree();
+            TreeWalk treeWalk = new TreeWalk(repository);
+            treeWalk.addTree(tree);
+            treeWalk.setRecursive(false);
+            while (treeWalk.next()) {
+                if (treeWalk.isSubtree()) {
+                    treeWalk.enterSubtree();
+                } else {
+                    files.put(treeWalk.getPathString(), treeWalk.getObjectId(0));
+                }
+            }
+            walk.dispose();
+
+        } catch (IncorrectObjectTypeException e) {
+            e.printStackTrace();
+        } catch (CorruptObjectException e) {
+            e.printStackTrace();
+        } catch (MissingObjectException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return files;
     }
 
     private void closeRepository() {
