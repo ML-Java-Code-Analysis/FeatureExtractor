bbf814f352ad15789c80aeb9db29358c2d977e68
Yacine Mekesser <ymekesser@hotmail.com>
2016-04-10 17:49:37 +0200

    Reflection eingebaut um Features dynamisch zu ermitteln.

--- src/main/java/FeatureExtractor.java
+++ src/main/java/FeatureExtractor.java
@@ -4,12 +4,18 @@
  * @author ymeke
  */
 
+import features.Feature;
 import model.Version;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.reflections.Reflections;
 import repository.Git;
 import utils.Config;
 
@@ -18,16 +24,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FeatureExtractor {
+    private static Logger logger;
 
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger("main");
+        logger = Logger.getLogger("main");
         logger.log(Level.INFO, "Starting Feature Extractor.");
 
         logger.log(Level.INFO, "Reading Arguments.");
         Config cfg = new Config();
         cfg.parse(args);
         logger.log(Level.INFO, "Reading Config Files.");
-        cfg.readConfigFile();
+        //cfg.readConfigFile();
 
         // TODO Relevante Fileversionen aus DB holen
         List<Version> versions = new ArrayList<Version>();
@@ -39,10 +46,12 @@ public class FeatureExtractor {
         try {
             git = new Git(repositoryPath);
         } catch (IOException e) {
-            //TODO: Exit program
             e.printStackTrace();
+            //TODO: Exit program
+            return;
         }
         // TODO Für jede Fileversion
+        List<Feature> features = getFeatures();
         for (Version version : versions) {
             String path = version.getFile().getPath();
             String commitId = version.getCommitId();
@@ -50,13 +59,18 @@ public class FeatureExtractor {
             try {
                 char[] code = git.getSourceCode(path, commitId);
                 System.out.println(code);
+                CompilationUnit ast = parse(code);
+                for (Feature feature : features) {
+                    double value = feature.extract(ast, code);
+                    String featureId = feature.getFeatureId();
+
+                    //FeatureValue.addOrUpdateFeatureValue(featureId, version.getId(), value);
+                }
+
             } catch (FileNotFoundException e) {
                 logger.log(Level.INFO, "File " + path + " was not found. Skipping this one.");
-                continue;
             }
 
-            // TODO - AST aus Code parsen
-            // TODO - Alle Feature Extractors drüberlaufen lassen
         }
 
         //  - File/Source-Code aus git holen (repository-package)
@@ -69,6 +83,40 @@ public class FeatureExtractor {
 
         git.closeRepository();
 
+    }
+
+    private static CompilationUnit parse(char[] code) {
+        ASTParser parser = ASTParser.newParser(AST.JLS8);
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+        parser.setSource(code);
+        parser.setResolveBindings(true);
+        return (CompilationUnit) parser.createAST(null);
+    }
+
+    private static List<Feature> getFeatures() {
+        List<Feature> features = new ArrayList<Feature>();
+        for (Class<? extends Feature> featureClass : getFeatureClasses()) {
+            Feature feature = null;
+            try {
+                feature = featureClass.newInstance();
+                features.add(feature);
+                logger.log(Level.INFO, "Instantiated Feature " + featureClass.getName());
+            } catch (InstantiationException e) {
+                String message = String.format("Could not instantiate Feature %s. Message%s",
+                        featureClass.getName(), e.getMessage());
+                logger.log(Level.WARNING, message);
+            } catch (IllegalAccessException e) {
+                String message = String.format("Could not instantiate Feature %s. Message%s",
+                        featureClass.getName(), e.getMessage());
+                logger.log(Level.WARNING, message);
+            }
+        }
+        return features;
+    }
+
+    private static Set<Class<? extends Feature>> getFeatureClasses() {
+        Reflections reflections = new Reflections("features");
+        return reflections.getSubTypesOf(Feature.class);
     }
 
 }
